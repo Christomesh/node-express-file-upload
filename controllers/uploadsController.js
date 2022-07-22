83d20db58097ec3661ca98116c82ad05f25125a4
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-07-22 15:12:37 +0100

    final commit

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -2,6 +2,7 @@ const path = require("path")
 const {StatusCodes} = require("http-status-codes")
 
 const CustomError = require("../errors")
+const cloudinary = require("cloudinary").v2
 const fs = require("fs")
 
 
@@ -23,11 +24,23 @@ const uploadsProductImageLocal = async (req, res)=>{
 
     const imagePath = path.join(__dirname, "../public/uploads/" + `${productImage.name}`)
     await productImage.mv(imagePath)
-    
+
     return res.status(StatusCodes.OK).json({image: {src: `/uploads/${productImage.name}`}})
 }
 
-module.exports = {
-    uploadsProductImageLocal,
-
-}
+//Best Alternative using clodinary
+const uploadProductImage = async (req, res) => {
+    const result = await cloudinary.uploader.upload(
+      req.files.image.tempFilePath,
+      {
+        use_filename: true,
+        folder: 'file-upload',
+      }
+    );
+    fs.unlinkSync(req.files.image.tempFilePath);
+    return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
+  };
+  
+  module.exports = {
+    uploadProductImage,
+  };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,9 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 const {createProduct, getAllProducts} = require("../controllers/productController")
-const {uploadsProductImageLocal} = require("../controllers/uploadsController")
+const {uploadProductImage} = require("../controllers/uploadsController")
 
 router.route('/').post(createProduct).get(getAllProducts)
-router.route('/uploads').post(uploadsProductImageLocal)
+router.route('/uploads').post(uploadProductImage)
 
 module.exports = router
